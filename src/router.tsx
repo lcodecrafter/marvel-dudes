c543926bf7c16dd1bb5c09cf1f63fa2d70b83a90
lCodecrafter <lcodecrafter@gmail.com>
2025-02-06 12:56:12 +0100

    Update project architecture
    Now it will be organized by features.

--- src/router.tsx
+++ src/router.tsx
@@ -1,13 +1,13 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
-import { Home } from '@/pages/home';
+import { CharactersList } from '@/features/characters/pages/CharactersList';
 import { Error } from '@/pages/error';
-import { Base } from './pages/base';
+import { Base } from './layouts/base';
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Base />} errorElement={<Error />}>
       <Route errorElement={<Error />}>
-        <Route index element={<Home />} />
+        <Route index element={<CharactersList />} />
       </Route>
     </Route>,
   ),
