f171c1812b8084ac50790eff3ee5e7e04495d1f1
lCodecrafter <lcodecrafter@gmail.com>
2025-02-06 17:27:12 +0100

    Renamed base.tsx to Base.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 import { CharactersList } from '@/features/characters/pages/CharactersList';
 import { Error } from '@/pages/error';
-import { Base } from './layouts/base';
+import { Base } from './layouts/Base';
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
